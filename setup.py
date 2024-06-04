70ba70493d29fa5f0318600c250cbf340ef75b20
shreehari-divate <shreeharidivate03@gmail.com>
2024-06-04 18:28:41 +0530

    Added comments

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
+'''
+* This file is used to package python libraries and applications.
+* Here it is useful for managing dependencies and ensuring consistent environment setup.
+* It tells setup tools how to build, install and package the project.
+* the setup function is entry point for setuptools. It takes arguments such as:
+  -> name
+  -> version of project
+  -> author
+  -> install_requires(this tells setuptools what packages our project requires)
+'''
+
+
+
 from typing import List
 from setuptools import find_packages, setup
 
 HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
-        This function will return the list of requirements
+       * This function reads the requirements from requirement.txt and returns them as list of strings
+       * It also removes -e. line from the list. This -e. tells pip to install package in edit mode.
     '''
     requirements=[]
     with open(file_path) as file_obj:
