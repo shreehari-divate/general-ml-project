e8735527c6518c8c9cffdde8d03997c8747366a2
shreehari-divate <shreeharidivate03@gmail.com>
2024-06-05 13:38:22 +0530

    Updates in logger and exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,12 @@
+
+
 import sys
 import logging 
+
+'''
+ The below function takes two arguments one for error message and a sys object that contains info about the error
+ It extracts the traceback object from the sys object, gets the filename and line number where the error occurred, and formats a detailed error message.
+'''
 
 def error_msg_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -17,3 +24,8 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_msg
 
+''''
+ * class CustomException(Exception): This class defines a custom exception. It inherits from the built-in Exception class, which means it behaves like a standard Python exception but can also have additional features.
+ * def __init__(self, error_msg, error_detail:sys): This is the constructor for the CustomException class. It takes an error message and a sys object as arguments, calls the error_msg_detail function to generate a detailed error message, and stores this message in the error_msg attribute.
+ * def __str__(self): This method returns a string representation of the CustomException object. When you print a CustomException object or convert it to a string, this method will be called, and the detailed error message will be returned.
+ '''
--- src/logger.py
+++ src/logger.py
@@ -3,8 +3,13 @@ import os
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# The above line creates a string for the logfile name using the current date and time.
+#The strftime function formats the date and time as a string in the format month_day_year_hour_minute_second.
+
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+#This line creates path for the log file. the os.path.join combines current working directory.
 os.makedirs(logs_path,exist_ok=True)
+#This line creates the directory for the log file if it doesn’t already exist. The exist_ok=True argument means that no error will be raised if the directory already exists.
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
@@ -13,3 +18,9 @@ logging.basicConfig(
     format="[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
+
+'''
+* The above function call configures the logging system. 
+* The filename argument specifies the file to write the log messages to.
+* The format argument specifies the format of the log messages, which includes the time of the message (%(asctime)s), the line number where the logging call was made (%(lineno)d), the name of the logger (%(name)s), the severity level of the message (%(levelname)s), and the message text (%(message)s). The level argument sets the root logger level to INFO, which means that only events of this severity and above will be tracked.
+'''
